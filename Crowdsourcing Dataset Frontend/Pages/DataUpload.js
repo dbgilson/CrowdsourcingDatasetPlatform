c3959b08787d6057421cda9b1414daa89166ef07
Gilson, David Bryon <dgilson@utk.edu>
2022-03-02 09:54:37 -0500

    Getting JS for Submitting tags + files in DatasetUpload

--- Crowdsourcing Dataset Frontend/Pages/DataUpload.js
+++ Crowdsourcing Dataset Frontend/Pages/DataUpload.js
@@ -4,3 +4,16 @@ $(document).ready(function () {
         $('#data').append('<li class="list-group-item w-50">' + $('#search').val() + '</li>')
     });
 });
+
+$(document).ready(function () {
+    $('#datasetSubmit').click(function () {
+        var list = document.getElementsByClassName('list-group-item w-50');
+        var listArray = [];
+        for (var i = 0; i < list.length; i++) {
+            listArray.push(list[i].innerHTML);
+        }
+        for (let i = 0; i < $('#data').find("li").length; i++) {
+            $('#dataRecieved').append('<li class="list-group-item w-50">' + listArray[i] + '</li>')
+        }
+    });
+})
