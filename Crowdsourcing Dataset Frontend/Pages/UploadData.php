36a01c686003eeadbd8561821b1129e3bc9ad887
dbgilson <73197003+dbgilson@users.noreply.github.com>
2022-04-21 22:27:11 -0400

    Updated UploadData.php for Formatting

--- Crowdsourcing Dataset Frontend/Pages/UploadData.php
+++ Crowdsourcing Dataset Frontend/Pages/UploadData.php
@@ -1,15 +1,76 @@
 <?php include('../../config/server.php') ?>
 <?php $_SESSION['dataset'] = "Plants"; // FIXME ?>
-<!DOCTYPE html>
-<html>
-<body>
 
-<form action="UploadData.php" method="post" enctype="multipart/form-data">
-    <?php include('../../config/errors.php'); ?>
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload Image" name="upload_image">
-</form>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Crowdsourcing Dataset Platform</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../bootstrap-5.1.3-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js" integrity="sha384-a5N7Y/aK3qNeh15eJKGWxsqtnX/wWdSZSKp+81YjTmS15nvnvxKHuzaWwXHDli+4" crossorigin="anonymous"></script>
+    <script src="DataUpload.js"></script>
+    <div class="container">
+        <style>
+            .bd-placeholder-img {
+                font-size: 1.125rem;
+                text-anchor: middle;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                user-select: none;
+            }
+
+            @media (min-width: 768px) {
+                .bd-placeholder-img-lg {
+                    font-size: 3.5rem;
+                }
+            }
+        </style>
+
+
+        <!-- Custom styles for this template -->
+        <link href="Login.css" rel="stylesheet">
+        <link href="headers.css" rel="stylesheet">
+</head>
+<body class="text-center">
+    <main>
+        <div class="container">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                    <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg>
+                    <span class="fs-4">CDP</span>
+                </a>
+
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Login</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Search</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link">Help</a></li>
+                </ul>
+            </header>
+        </div>
+        <?php
+            echo '<h1 align="left" class="h4 mt-1 mb-5 fw-normal">Current User: ' . $_SESSION['username'] . '</h1>';
+        ?>
+        <form class="form-upload" action="UploadData.php" method="post" enctype="multipart/form-data">
+        <?php include('../../config/errors.php'); ?>
+            <div class="mb-3">
+                <label for="formFile" class="form-label">Select Images to Upload</label>
+                <input class="form-control" type="file" name="fileToUpload" id="fileToUpload" multiple>
+            </div>
+
+            <input type="submit" class="w-75 btn btn-lg btn-primary" value="Upload Image" name="upload_image">
+
+            <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        </form>
+    </main>
+
+
 
 </body>
 </html>
