52852b57b2911b37622989ad688ab5031a1fffdd
arhencey <arhencey@yahoo.com>
2022-05-03 22:21:39 -0400

    created a page that demonstrates the functionality of the download_dataset method

---
+++ Crowdsourcing Dataset Frontend/Pages/DownloadDataset.php
@@ -0,0 +1,9 @@
+<?php
+include('../../config/server.php');
+
+echo '<form name="download_dataset" method="post">';
+echo '<button type="submit" class="btn btn-info" name="download_dataset">Download Zip</button>';
+echo '</form>';
+
+
+?>
